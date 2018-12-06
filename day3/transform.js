8fd1cc35f97a294510e2e9846cf5868d9e001123
Ryan <ryan.yee@optimizely.com>
2018-12-06 11:30:25 -0800

    Update day3 transfrom module

--- day3/transform.js
+++ day3/transform.js
@@ -1,10 +1,9 @@
 const fs = require('fs')
 
-fs.readFile('./day3/data.txt', { encoding: 'utf8'}, function (error, data) {
+fs.readFile('./data.txt', { encoding: 'utf8'}, function (error, data) {
   let transform = data.split('\n').map(line => {
     const [_, x, y, w, h] = /#\d*\s@\s(\d*),(\d*):\s(\d*)x(\d*)/.exec(line)
     return `[${[x, y, w, h]}]`
   })
-  console.log(transform)
-  fs.writeFileSync('./day3/coordinates.js', transform)
+  fs.writeFileSync('./data.js', transform)
 })
